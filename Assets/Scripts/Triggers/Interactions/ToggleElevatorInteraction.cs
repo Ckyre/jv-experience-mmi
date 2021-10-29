cf5c4485eb42f4d57493486ebe0ecbee7b8087e5
Gabriel <ckyre.game@gmail.com>
2021-10-30 01:53:34 +0200

    fix

--- Assets/Scripts/Triggers/Interactions/ToggleElevatorInteraction.cs
+++ Assets/Scripts/Triggers/Interactions/ToggleElevatorInteraction.cs
@@ -6,20 +6,29 @@ public class ToggleElevatorInteraction : Interactable
     [SerializeField] private GameObject leverMesh;
     [SerializeField] private AudioSource source;
     [SerializeField] private AudioClip toggleSound;
+    [SerializeField] private bool active = true;
 
     private bool toggle = true;
 
     public override void OnPlayerInteract()
     {
-        base.OnPlayerInteract();
+        if (active)
+        {
+            base.OnPlayerInteract();
 
-        elevator.Toggle();
-        source.PlayOneShot(toggleSound);
+            elevator.Toggle();
+            source.PlayOneShot(toggleSound);
 
-        toggle = !toggle;
-        if (toggle)
-            leverMesh.transform.eulerAngles += new Vector3(90f, 0, 0);
-        else
-            leverMesh.transform.eulerAngles -= new Vector3(90f, 0, 0);
+            toggle = !toggle;
+            if (toggle)
+                leverMesh.transform.eulerAngles += new Vector3(90f, 0, 0);
+            else
+                leverMesh.transform.eulerAngles -= new Vector3(90f, 0, 0);
+        }
+    }
+
+    public void Active (bool value)
+    {
+        active = value;
     }
 }
